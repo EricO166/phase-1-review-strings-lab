ed0b5b6dbf1cd99c1b148c0ca2446e20d4670315
EricO166 <ochielsenor@gmail.com>
2025-06-17 12:54:19 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// Define the current user
+
 const currentUser = 'Grace';
 
-// Welcome message using currentUser
+
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-// Excited welcome message in all caps
+
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
-// Short greeting using only the first letter of the user's name
+
 const shortGreeting = `Welcome, ${currentUser[0]}!`;
